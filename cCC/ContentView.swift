f1db70623c58d59255bb36a38325b862bb4e575f
noemirtil <mirtil.noe@gmail.com>
2025-05-12 20:33:06 +0200

    creating the repository

---
+++ cCC/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  camelCaseConv
+//
+//  Created by Germaine on 4/5/25.
+//
+
+import SwiftUI
+
+
+/*
+ Form: check https://www.swiftbeta.com/textfield-en-swiftui/
+ Empezar ahí:
+ */
+
+
+struct ContentView: View {
+	@State private var phrase: String = ""
+	var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					Text("Hello, world!")
+				}
+				Section {
+					TextField("Enter a phrase", text: $phrase)
+				}
+				Section {
+					Text("camelCase result")
+					Text("Hello, world!")
+				}
+			}
+			.navigationTitle("SwiftUI")
+			.navigationBarTitleDisplayMode(.inline)
+		}
+	}
+}
+
+
+//MARK: - BACKEND
+
+// camelCase converter by Noemí
+// Check https://convertcase.net/
+
+
+
+//MARK: - PREVIEWS
+
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
---
+++ cCC/cCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  cCCappApp.swift
+//  cCCapp
+//
+//  Created by Germaine on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct cCCappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
